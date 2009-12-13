4442a9f248054eca981676bb8d72e21d5e1611e9
bender.ross <bender.ross@6538fbfe-e79f-11de-9298-fbd6a473cdd1>
2009-12-13 17:26:40 +0000

    Initial system-reads in verses from a file and creates Verse objects

---
+++ Topical Memory System/Topical Memory System/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Topical_Memory_System
+{
+	public partial class MenuExit : Form
+	{
+		public MenuExit()
+		{
+			InitializeComponent();
+		}
+
+		private void MenuExitClick(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void ReadFromFile(object sender, EventArgs e)
+		{
+			StreamReader SR;
+			string S;
+			SR = File.OpenText("../../../tmsVerses-ESV.txt");
+			S = SR.ReadLine();
+			while (S != null)
+			{
+				if (S.Trim().Length > 0)
+				{
+					string[] info = S.Split('/');
+					Verse v = new Verse(info[0], Convert.ToInt32(info[1].Split(':')[0]), info[1].Split(':')[1],
+						info[2].Split(' ')[0], Convert.ToInt32(info[2].Split(' ')[1]), info[3]);
+					Debug.WriteLine(v.getReference());
+					Debug.WriteLine(v.getVerseData());
+					S = SR.ReadLine();
+				}
+				else
+				{
+					S = SR.ReadLine();
+				}
+			}
+			SR.Close();
+		}
+	}
+}
